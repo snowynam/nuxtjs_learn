495ac151146483b7f666f29b3bb8783281608c68
ynyoon <ynyoon@enliple.com>
2022-09-01 14:19:02 +0900

    init

---
+++ layouts/error.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1 v-if="error.statusCode === 404">Page not found</h1>
+    <h1 v-else>An error occured</h1>
+    <p>페이지를 찾을 수 없습니다.</p>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['error'],
+    layout: 'error' // you can set a custom layout for the error page
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/main.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <NuxtLogo/>
+    <h1>메인 페이지2</h1>
+  </div>
+</template>
+
+<script>
+import NuxtLogo from '../components/NuxtLogo.vue';
+    
+export default {
+  components:{
+    NuxtLogo,
+  }
+}
+</script>
+
+<style>
+
+</style>
