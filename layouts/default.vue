27e623c161aa949c08b4a993b73f228dcf4018a8
ynyoon <ynyoon@enliple.com>
2022-09-01 18:56:05 +0900

    .

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
-    <h1>{{$route.name}} 페이지</h1>
+    <header>
+      <h1>{{$route.name}} 페이지</h1>
+      <nuxtLink to="/">홈페이지</nuxtLink>
+      <nuxtLink to="/main">메인 페이지</nuxtLink>
+      <nuxtLink to="/product">상품 페이지</nuxtLink>
+      <!-- <nuxtLink></nuxtLink> == <router-link></router-link> -->
+    </header>
     <Nuxt />
+    <!-- <Nuxt /> == <router-view></router-view> -->
   </div>
 </template>
 
