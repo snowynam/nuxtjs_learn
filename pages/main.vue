6bea95c90ba88816a2bf5da1c291c218097e7cf9
ynyoon <ynyoon@enliple.com>
2022-09-02 10:24:21 +0900

    .

--- pages/main.vue
+++ pages/main.vue
@@ -13,15 +13,16 @@ import axios from 'axios';
 export default {
   async asyncData(){
     const response = await axios.get('http://localhost:3000/products')
-    console.log(response)
+   // console.log(response)
     const products = response.data
     return { products }
   },
-  // data() {
-  //   return {
-  //     products: []
-  //   }
-  // },
+  data() {
+    return {
+      products: []
+    }
+  },
+  
   
   // async created() {
     
