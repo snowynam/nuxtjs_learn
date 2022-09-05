43db59e7aafbe698f606535138c5a924382147cc
snowynam <snowynam1@gmail.com>
2022-09-06 01:22:55 +0900

    상품상세 구현 사전작업

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
     <header>
-      <h1>{{$route.name}} 페이지</h1>
+      <h1>Nuxt Shopping</h1>
+      <!-- <h1>{{$route.name}} 페이지</h1>
       <nuxtLink to="/">홈페이지</nuxtLink>
       <nuxtLink to="/main">메인 페이지</nuxtLink>
-      <nuxtLink to="/product">상품 페이지</nuxtLink>
+      <nuxtLink to="/product">상품 페이지</nuxtLink> -->
       <!-- <nuxtLink></nuxtLink> == <router-link></router-link> -->
     </header>
     <Nuxt />
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,80 @@
 <template>
-  <div>
-    <NuxtLogo/>
-    <p>넉스트 시작하기</p>
+  <div class="app">
+    <main>
+      <div>
+        <input type="text" />
+      </div>
+      <ul>
+        <li class="item flex" v-for="product in products" :key="product.id" @click="moveToDetailPage(product.id)">
+          <img class="product-image" :src="product.imageUrl" :alt="product.name">
+          <p>{{product.name}}</p>
+          <span>{{product.price}}</span>
+        </li>
+      </ul>
+    </main>
   </div>
 </template>
-
-<script>
-export default {
   
+<script>
+import axios from 'axios';
+
+export default {
+  async asyncData(){
+    const response = await axios.get('http://localhost:3000/products')
+    console.log(response)
+    const products = response.data.map((item)=>({
+      ...item,
+      imageUrl:`${item.imageUrl}?random=${Math.random()}`
+    }))
+    return { products }
+  },
+  data() {
+    return {
+      products: []
+    }
+  },
+  methods: {
+    moveToDetailPage(id) {
+      console.log(id)
+    }
+  }
+  
+  // async created() {
+    
+  // }
 }
 </script>
-<style></style>
+<style scoped>
+.flex {
+  display: flex;
+  justify-content: center;
+}
+.item {
+  display: inline-block;
+  width: 400px;
+  height: 300px;
+  text-align: center;
+  margin: 0 0.5rem;
+  cursor: pointer;
+}
+.product-image {
+  width: 400px;
+  height: 250px;
+}
+.app {
+  position: relative;
+}
+.cart-wrapper {
+  position: sticky;
+  float: right;
+  bottom: 50px;
+  right: 50px;
+}
+.cart-wrapper .btn {
+  display: inline-block;
+  height: 40px;
+  font-size: 1rem;
+  font-weight: 500;
+}
+</style>
+  
