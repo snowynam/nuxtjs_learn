68af9b0cb00c909ed5d859d7862c2634684be376
snowynam <snowynam1@gmail.com>
2022-09-06 01:09:19 +0900

    상품목록 표시

--- components/ProductList.vue
+++ components/ProductList.vue
@@ -3,8 +3,15 @@
 </template>
 
 <script>
-export default {
+import axios from 'axios'
 
+export default {
+  async asyncData(){
+    const response = await axios.get('http://localhost:3000/products')
+   // console.log(response)
+    const products = response.data
+    return { products }
+  },
 }
 </script>
 
--- pages/main.vue
+++ pages/main.vue
@@ -2,7 +2,14 @@
     <div>
       <p>메인 페이지 입니다.</p>
       <div>
-        {{products}}
+        <ul>
+          <li v-for="product in products" :key="product.id">
+            <!-- {{product}} -->
+            <img :src="product.imageUrl" :alt="product.name">
+            <p>{{product.name}}</p>
+            <p>{{product.price}}</p>
+          </li>
+        </ul>
       </div>
     </div>
 </template>
@@ -13,8 +20,11 @@ import axios from 'axios';
 export default {
   async asyncData(){
     const response = await axios.get('http://localhost:3000/products')
-   // console.log(response)
-    const products = response.data
+    console.log(response)
+    const products = response.data.map((item)=>({
+      ...item,
+      imageUrl:`${item.imageUrl}?random=${Math.random()}`
+    }))
     return { products }
   },
   data() {
